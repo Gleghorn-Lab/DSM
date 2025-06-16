fd7d77c2c8580f7fe018013dd259dcfb17db5dfa
Logan Hallee <lhallee@udel.edu>
2025-06-16 15:08:55 -0400

    Update data_collators.py

--- data/data_collators.py
+++ data/data_collators.py
@@ -1,8 +1,10 @@
 import torch
 from typing import Tuple, List, Dict, Union, Any
 
-from models.alignment_helpers import AlignmentScorer
-from data.utils import ProteinMasker
+try:
+    from data.utils import ProteinMasker
+except:
+    from .utils import ProteinMasker
 
 
 def standard_data_collator(batch):
@@ -143,119 +145,6 @@ class PairCollator_ab:
             'input_ids_a': tokenized_a['input_ids'],
             'input_ids_b': tokenized_b['input_ids'],
             'attention_mask_a': tokenized_a['attention_mask'],
-            'attention_mask_b': tokenized_b['attention_mask'],
-            'labels': labels
-        }
-
-
-class NWCollatorFull:
-    def __init__(self, tokenizer, max_length=2048, asinh=False, **kwargs):
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-        self.scorer = AlignmentScorer()
-        self.asinh = asinh
-
-    def __call__(self, batch: List[Tuple[str, str]]) -> Dict[str, torch.Tensor]:
-        seqs_a, seqs_b = zip(*batch)
-        
-        # Truncate sequences if their combined length exceeds max_length
-        truncated_seqs_a = []
-        truncated_seqs_b = []
-        for seq_a, seq_b in zip(seqs_a, seqs_b):
-            # Make copies to avoid modifying the original sequences
-            trunc_a, trunc_b = seq_a, seq_b
-            
-            # Use a while loop to gradually truncate sequences
-            while len(trunc_a) + len(trunc_b) > self.max_length:
-                # Determine which sequence is longer
-                if len(trunc_a) > len(trunc_b):
-                    # Remove two characters from the longer sequence
-                    trunc_a = trunc_a[:-2]
-                elif len(trunc_b) > len(trunc_a):
-                    # Remove two characters from the longer sequence
-                    trunc_b = trunc_b[:-2]
-                else:
-                    # If both sequences are the same length, remove from both
-                    trunc_a = trunc_a[:-2]
-                    trunc_b = trunc_b[:-2]
-            
-            truncated_seqs_a.append(trunc_a)
-            truncated_seqs_b.append(trunc_b)
-        
-        # Calculate NW scores using the truncated sequences
-        labels = [self.scorer(seq_a, seq_b) for seq_a, seq_b in zip(truncated_seqs_a, truncated_seqs_b)]
-        labels = torch.tensor(labels, dtype=torch.float32)
-        if self.asinh:
-            labels = torch.asinh(labels)
-        
-        # Tokenize the truncated sequences
-        tokenized = self.tokenizer(
-            truncated_seqs_a, truncated_seqs_b,
-            padding='longest',
-            return_tensors='pt'
-        )
-        return {
-            'input_ids': tokenized['input_ids'],
-            'attention_mask': tokenized['attention_mask'],
-            'labels': labels
-        }
-
-
-class NWCollatorCross:
-    def __init__(self, tokenizer, max_length=2048, asinh=False, **kwargs):
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-        self.scorer = AlignmentScorer()
-        self.asinh = asinh
-
-    def __call__(self, batch: List[Tuple[str, str]]) -> Dict[str, torch.Tensor]:
-        seqs_a, seqs_b = zip(*batch)
-        
-        # Truncate sequences if their combined length exceeds max_length
-        truncated_seqs_a = []
-        truncated_seqs_b = []
-        for seq_a, seq_b in zip(seqs_a, seqs_b):
-            # Make copies to avoid modifying the original sequences
-            trunc_a, trunc_b = seq_a, seq_b
-            
-            # Use a while loop to gradually truncate sequences
-            while len(trunc_a) + len(trunc_b) > self.max_length:
-                # Determine which sequence is longer
-                if len(trunc_a) > len(trunc_b):
-                    # Remove two characters from the longer sequence
-                    trunc_a = trunc_a[:-2]
-                elif len(trunc_b) > len(trunc_a):
-                    # Remove two characters from the longer sequence
-                    trunc_b = trunc_b[:-2]
-                else:
-                    # If both sequences are the same length, remove from both
-                    trunc_a = trunc_a[:-2]
-                    trunc_b = trunc_b[:-2]
-            
-            truncated_seqs_a.append(trunc_a)
-            truncated_seqs_b.append(trunc_b)
-        
-        # Calculate NW scores using the truncated sequences
-        labels = [self.scorer(seq_a, seq_b) for seq_a, seq_b in zip(truncated_seqs_a, truncated_seqs_b)]
-        labels = torch.tensor(labels, dtype=torch.float32)
-        if self.asinh:
-            labels = torch.asinh(labels)
-        
-        # Tokenize the truncated sequences
-        tokenized_a = self.tokenizer(
-            truncated_seqs_a,
-            padding='longest',
-            return_tensors='pt'
-        )
-        tokenized_b = self.tokenizer(
-            truncated_seqs_b,
-            padding='longest',
-            return_tensors='pt'
-        )
-        return {
-            'input_ids_a': tokenized_a['input_ids'],
-            'attention_mask_a': tokenized_a['attention_mask'],
-            'input_ids_b': tokenized_b['input_ids'],
             'attention_mask_b': tokenized_b['attention_mask'],
             'labels': labels
         }
